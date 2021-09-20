fd52779a1a1b050800d84c084e2ca17e0c09bdca
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-20 20:50:35 +0300

    add CoffeeItem to Content

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,5 +1,11 @@
+import CoffeeItem from './CoffeeItem'
+
 const Content = () => {
-  return <div className="content">content</div>
+  return (
+    <div className="content">
+      <CoffeeItem />
+    </div>
+  )
 }
 
 export default Content

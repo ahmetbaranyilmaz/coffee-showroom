e2c923646740eb96afa3856a14c201a968823a83
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-20 20:46:46 +0300

    add CategoryList component to navigation bar

--- src/components/navigation-bar/NavigationBar.js
+++ src/components/navigation-bar/NavigationBar.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import CategoryList from './CategoryList'
 import Input from './Input'
 
 const NavigationBar = () => {
   return (
     <div className="nav">
       <Input />
+      <CategoryList />
     </div>
   )
 }

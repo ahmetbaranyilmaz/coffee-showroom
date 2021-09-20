2ed6bbc02acf2fb70750706d320933d34e45e681
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-20 20:55:11 +0300

    add title description ingredient to CoffeeItem

--- src/components/content/CoffeeItem.js
+++ src/components/content/CoffeeItem.js
@@ -1,5 +1,11 @@
 const CoffeeItem = () => {
-  return <div className="coffee-item">item</div>
+  return (
+    <div className="coffee-item">
+      <h3 className="title">title</h3>
+      <span className="description">description</span>
+      <span className="ingredient">ingredients</span>
+    </div>
+  )
 }
 
 export default CoffeeItem

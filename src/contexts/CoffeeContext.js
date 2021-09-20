a0d0235afcdcf74f939bc9a93f42af1a66fb1b2e
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-20 20:24:25 +0300

    add CoffeeContext

---
+++ src/contexts/CoffeeContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext, useState } from 'react'
+import CoffeeData from '../data/CoffeeData'
+
+export const CoffeeContext = createContext()
+export const useCoffee = () => useContext(CoffeeContext)
+
+export const CoffeeContextProvider = ({ children }) => {
+  const coffeeData = CoffeeData
+  const [coffees, setCoffees] = useState(coffeeData)
+
+  const contextValue = {
+    coffeeData,
+    coffees,
+    setCoffees
+  }
+
+  return (
+    <CoffeeContext.Provider value={contextValue}>
+      {children}
+    </CoffeeContext.Provider>
+  )
+}

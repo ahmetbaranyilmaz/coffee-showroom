c4131186dce4456978eb65226f8c51e8f5a0ff53
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-20 21:10:52 +0300

    add filter by category function

--- src/components/navigation-bar/CategoryListItem.js
+++ src/components/navigation-bar/CategoryListItem.js
@@ -1,5 +1,25 @@
+import { useCoffee } from '../../contexts/CoffeeContext'
+
 const CategoryListItem = ({ text }) => {
-  return <li className="category-item">{text}</li>
+  const { coffeeData, setCoffees } = useCoffee()
+
+  const handleClick = (e) => {
+    const category = e.target.innerText
+    if (category === 'All Coffees') {
+      setCoffees(coffeeData)
+    } else {
+      const newCoffees = coffeeData.filter(
+        (coffee) => coffee.category === category.toLowerCase()
+      )
+      setCoffees(newCoffees)
+    }
+  }
+
+  return (
+    <li className="category-item" onClick={handleClick}>
+      {text}
+    </li>
+  )
 }
 
 export default CategoryListItem

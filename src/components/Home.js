3c7a043af2b85f4b2a0f98831d99a538bdce1e9d
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-20 20:52:02 +0300

    add Content to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
+import Content from './content/Content'
 import NavigationBar from './navigation-bar/NavigationBar'
 
 const Home = () => {
   return (
     <div className="home">
       <NavigationBar />
+      <Content />
     </div>
   )
 }

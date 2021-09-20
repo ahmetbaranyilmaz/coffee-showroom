b1e1068cec90446be2928edca9b8aa2fb2fc1136
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-20 20:39:05 +0300

    add input to nav-bar

--- src/components/navigation-bar/NavigationBar.js
+++ src/components/navigation-bar/NavigationBar.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import Input from './Input'
 
 const NavigationBar = () => {
-  return <div className="nav">NavigationBar</div>
+  return (
+    <div className="nav">
+      <Input />
+    </div>
+  )
 }
 
 export default NavigationBar

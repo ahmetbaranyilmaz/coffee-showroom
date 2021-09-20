a881f9160bb01059b93f848ffb8a0187ba543760
Baran YILMAZ <abybaranyilmaz@gmail.com>
2021-09-20 21:06:09 +0300

    add search function

--- src/components/navigation-bar/Input.js
+++ src/components/navigation-bar/Input.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import { useCoffee } from '../../contexts/CoffeeContext'
 
 const Input = () => {
-  return <input className="input" type="text" placeholder="Search" />
+  const { coffeeData, setCoffees } = useCoffee()
+
+  const handleChange = (e) => {
+    const title = e.target.value
+    console.log(title)
+    const newCoffee = coffeeData.filter((coffee) =>
+      coffee.title.toUpperCase().includes(title.toUpperCase())
+    )
+    setCoffees(newCoffee)
+  }
+  return (
+    <input
+      className="input"
+      type="text"
+      placeholder="Search"
+      onChange={handleChange}
+    />
+  )
 }
 
 export default Input
